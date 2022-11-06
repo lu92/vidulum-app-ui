1372c10bc29ef1723936a70dc8e189956a85e3fe
lu92 <biklucjan@gmail.com>
2022-11-06 21:38:25 +0100

    VID-UI-2: Create routing for empty home and quotation pages (#2)

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomePageComponent } from './pages/home-page/home-page.component';
+import { QuotationPageComponent } from './pages/quotation-page/quotation-page.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: 'home',
+    component: HomePageComponent,
+  },
+  {
+      path: 'quotation',
+      component: QuotationPageComponent,
+    }
+  ];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
