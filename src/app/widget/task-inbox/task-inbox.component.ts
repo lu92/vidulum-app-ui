e89a029efd5bf182ea66dc5d172b3561899ff2fc
lu92 <biklucjan@gmail.com>
2022-11-12 21:29:39 +0100

    VID-UI-4: Add Task-Inbox Widget

--- src/app/widget/task-inbox/task-inbox.component.ts
+++ src/app/widget/task-inbox/task-inbox.component.ts
@@ -12,27 +12,6 @@ export interface Task {
   created: Date;
   dueDate: Date;
 }
-
-export interface PeriodicElement {
-  name: string;
-  position: number;
-  weight: number;
-  symbol: string;
-}
-
-const ELEMENT_DATA: PeriodicElement[] = [
-  {position: 1, name: 'Hydrogen', weight: 1.0079, symbol: 'H'},
-  {position: 2, name: 'Helium', weight: 4.0026, symbol: 'He'},
-  {position: 3, name: 'Lithium', weight: 6.941, symbol: 'Li'},
-  {position: 4, name: 'Beryllium', weight: 9.0122, symbol: 'Be'},
-  {position: 5, name: 'Boron', weight: 10.811, symbol: 'B'},
-  {position: 6, name: 'Carbon', weight: 12.0107, symbol: 'C'},
-  {position: 7, name: 'Nitrogen', weight: 14.0067, symbol: 'N'},
-  {position: 8, name: 'Oxygen', weight: 15.9994, symbol: 'O'},
-  {position: 9, name: 'Fluorine', weight: 18.9984, symbol: 'F'},
-  {position: 10, name: 'Neon', weight: 20.1797, symbol: 'Ne'},
-];
-
   const TASKS: Task[] = [
     {taskId : "T101", userId : "lu92", name: 'Task1', description: "Hello world", status : "Open", created: new Date(2018, 0O5, 0O5, 17, 23, 42, 11), dueDate: new Date(2018, 0O5, 0O5, 17, 23, 42, 11)},
     {taskId : "T102", userId : "lu92", name: 'Task2', description: "Hello world", status : "Open", created: new Date(2018, 0O5, 0O5, 17, 23, 42, 11), dueDate: new Date(2018, 0O5, 0O5, 17, 23, 42, 11)},
@@ -51,7 +30,7 @@ export class TaskInboxComponent implements OnInit {
   ngOnInit(): void {
   }
 
-   displayedColumns: string[] = ['select', 'taskId', 'userId', 'name', 'symbol', 'created', 'dueDate'];
+   displayedColumns: string[] = ['select', 'taskId', 'userId', 'name', 'symbol', 'created', 'dueDate', 'action'];
     dataSource = new MatTableDataSource<Task>(TASKS);
     selection = new SelectionModel<Task>(true, []);
 
