7eda2e89e94a931bdaa408d368355e1cc7585af4
lu92 <biklucjan@gmail.com>
2022-11-14 03:07:15 +0100

    VID-UI-4: Add Task-Inbox Widget

--- src/app/widget/task-inbox/task-inbox.component.ts
+++ src/app/widget/task-inbox/task-inbox.component.ts
@@ -12,6 +12,7 @@ enum Priority {
 }
 export interface Task {
   taskId: string;
+  createdBy: string;
   userId: string;
   name: string;
   description: string;
@@ -24,11 +25,11 @@ export interface Task {
   dueDate: Date;
 }
   const TASKS: Task[] = [
-    {taskId : "9bb35d32-d516-44d9-a773-366ab4623b3b", userId : "lu92", name: 'Task1', description: "Hello world", isFavourite: true, isViewed: true, status : "Open", portfolioName: "PM portfolio",priority: Priority.Highest, created: new Date(2018, 0O5, 0O5, 17, 23, 42, 11), dueDate: new Date(2018, 0O5, 0O5, 17, 23, 42, 11)},
-    {taskId : "a6cb3b92-f751-4a1d-abfd-cc0b1c3c8f89", userId : "lu92", name: 'Task1', description: "Hello world", isFavourite: false, isViewed: true, status : "Open", portfolioName: "crypto portfolio",priority: Priority.High, created: new Date(2018, 0O5, 0O5, 17, 23, 42, 11), dueDate: new Date(2018, 0O5, 0O5, 17, 23, 42, 11)},
-    {taskId : "2452b778-a58d-45b6-81c7-c5bfb5a5a772", userId : "lu92", name: 'Task2', description: "Hello world", isFavourite: false, isViewed: false, status : "Open", portfolioName: "ETF portfolio",priority: Priority.Medium, created: new Date(2018, 0O5, 0O5, 17, 23, 42, 11), dueDate: new Date(2018, 0O5, 0O5, 17, 23, 42, 11)},
-    {taskId : "16c78fb8-d55b-4ba6-a299-4d4ef3d37768", userId : "lu92", name: 'Task3', description: "Hello world", isFavourite: true, isViewed: false, status : "Open", portfolioName: "Stock portfolio",priority: Priority.Low, created: new Date(2018, 0O5, 0O5, 17, 23, 42, 11), dueDate: new Date(2018, 0O5, 0O5, 17, 23, 42, 11)},
-    {taskId : "ce081cf4-1641-4384-b13e-d30fb4d08389", userId : "lu92", name: 'Task3', description: "Hello world", isFavourite: false, isViewed: true, status : "Open", portfolioName: "retirement portfolio", priority: Priority.Lowest, created: new Date(2018, 0O5, 0O5, 17, 23, 42, 11), dueDate: new Date(2018, 0O5, 0O5, 17, 23, 42, 11)}
+    {taskId : "9bb35d32-d516-44d9-a773-366ab4623b3b", createdBy: "MANUAL", userId : "lu92", name: 'Task1', description: "Hello world", isFavourite: true, isViewed: true, status : "Open", portfolioName: "PM portfolio",priority: Priority.Highest, created: new Date(2018, 0O5, 0O5, 17, 23, 42, 11), dueDate: new Date(2018, 0O5, 0O5, 17, 23, 42, 11)},
+    {taskId : "a6cb3b92-f751-4a1d-abfd-cc0b1c3c8f89", createdBy: "Rule: 1234556", userId : "lu92", name: 'Task1', description: "Hello world", isFavourite: false, isViewed: true, status : "Open", portfolioName: "crypto portfolio",priority: Priority.High, created: new Date(2018, 0O5, 0O5, 17, 23, 42, 11), dueDate: new Date(2018, 0O5, 0O5, 17, 23, 42, 11)},
+    {taskId : "2452b778-a58d-45b6-81c7-c5bfb5a5a772", createdBy: "MANUAL", userId : "lu92", name: 'Task2', description: "Hello world", isFavourite: false, isViewed: false, status : "Open", portfolioName: "ETF portfolio",priority: Priority.Medium, created: new Date(2018, 0O5, 0O5, 17, 23, 42, 11), dueDate: new Date(2018, 0O5, 0O5, 17, 23, 42, 11)},
+    {taskId : "16c78fb8-d55b-4ba6-a299-4d4ef3d37768", createdBy: "Rule: 1234557", userId : "lu92", name: 'Task3', description: "Hello world", isFavourite: true, isViewed: false, status : "Open", portfolioName: "Stock portfolio",priority: Priority.Low, created: new Date(2018, 0O5, 0O5, 17, 23, 42, 11), dueDate: new Date(2018, 0O5, 0O5, 17, 23, 42, 11)},
+    {taskId : "ce081cf4-1641-4384-b13e-d30fb4d08389", createdBy: "Rule: 1234558", userId : "lu92", name: 'Task3', description: "Hello world", isFavourite: false, isViewed: true, status : "Open", portfolioName: "retirement portfolio", priority: Priority.Lowest, created: new Date(2018, 0O5, 0O5, 17, 23, 42, 11), dueDate: new Date(2018, 0O5, 0O5, 17, 23, 42, 11)}
   ];
 
 @Component({
@@ -43,7 +44,7 @@ export class TaskInboxComponent implements OnInit {
   ngOnInit(): void {
   }
 
-   displayedColumns: string[] = ['select','star', 'menu', 'visibility', 'taskId', 'userId', 'name', 'status', 'portfolio', 'priority', 'created'];
+   displayedColumns: string[] = ['select','star', 'menu', 'visibility', 'taskId', 'createdBy', 'userId', 'name', 'status', 'portfolio', 'priority', 'created'];
     dataSource = new MatTableDataSource<Task>(TASKS);
     selection = new SelectionModel<Task>(true, []);
 
