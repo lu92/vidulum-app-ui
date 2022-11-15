0dbf90582d786f962368c706c457e423526a857b
lu92 <biklucjan@gmail.com>
2022-11-15 01:55:58 +0100

    VID-UI-4: Add Task-Inbox Widget

--- src/app/widget/rag-status/rag-status.component.ts
+++ src/app/widget/rag-status/rag-status.component.ts
@@ -1,17 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, Input, OnInit } from '@angular/core';
+import { RagStatus } from '../../model/RagStatus';
 
 @Component({
   selector: 'rag-status',
   templateUrl: './rag-status.component.html',
   styleUrls: ['./rag-status.component.scss']
 })
-export class RagStatusComponent implements OnInit {
+export class RagStatusComponent  {
 
-//   @Input()
-//   rag: RagStatus;
-  constructor() { }
+  @Input() rag!: RagStatus;
 
-  ngOnInit(): void {
+  fun() : string {
+    switch(Number(this.rag)) {
+
+      case RagStatus.RED:
+        return "rag-red";
+
+      case RagStatus.AMBER:
+             return "rag-amber";
+
+      case RagStatus.GREEN:
+             return "rag-green";
+
+       default:
+        return "rag-no-color";
+    }
   }
-
 }
